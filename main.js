6f4d3230e5f1ae01e432f9879297c9d0102d3457
YuriNaoki <dev.peterson.machado@gmail.com>
2024-02-19 15:15:35 -0300

    Alterações

--- main.js
+++ main.js
@@ -1,19 +1,18 @@
-        // Quando o usuário rola a página, execute a função myFunction
-        window.onscroll = function () {
-            myFunction();
-          };
-  
-          // Obtenha o navbar
-          var navbar = document.querySelector(".navbar");
-  
-          // Obtenha a posição offset do navbar
-          var sticky = navbar.offsetTop;
-  
-          // Adicione a classe sticky ao navbar quando você alcançar sua posição de rolagem. Remova "sticky" quando você deixar a posição de rolagem
-          function myFunction() {
-            if (window.pageYOffset >= sticky) {
-              navbar.classList.add("sticky");
-            } else {
-              navbar.classList.remove("sticky");
-            }
-          }
+var title1 = "Gestão de estoque como você nunca viu.";
+var title2 = "Emissão de nota fiscal pode ser simples.";
+var text1 = "A Digital Sistema simplifica: cadastro de fornecedores, relatórios de estoque,controle de compras, gestão de depósitos e mais! Otimize seu negócio conosco. Estamosaqui para tornar a gestão do seu negócio mais eficiente e descomplicada.";
+var text2 = "Com a Digital Sistema, sua agilidade para emitir os três principais tipos de notasfiscais (NFe, NFSe e NFCe) é incomparável. Elimine a necessidade de inserir dados repetidamente, deixe que os impostos sejam calculados automaticamente e desfrute demuito mais praticidade! Descubra como podemos otimizar o seu processo. Clique agorano botão abaixo para saber mais.";
+
+function changeText1() {
+  document.getElementById("myH2").innerHTML = title1;
+  document.getElementById("myP").innerHTML = text1;
+  document.getElementById("btn1").classList.add("btnAtive");
+  document.getElementById("btn2").classList.remove("btnAtive");
+}
+
+function changeText2() {
+  document.getElementById("myH2").innerHTML = title2;
+  document.getElementById("myP").innerHTML = text2;
+  document.getElementById("btn1").classList.remove("btnAtive");
+  document.getElementById("btn2").classList.add("btnAtive");
+}
