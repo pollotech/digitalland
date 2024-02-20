c9d41acd8b61eda45c79cc68762abb47911055b1
Antônio Henrique Pacheco Costa <102102220+henriquef9@users.noreply.github.com>
2024-02-20 13:44:06 -0300

    alterações

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var title1 = "Gestão de estoque como você nunca viu.";
-var title2 = "Emissão de nota fiscal pode ser simples.";
+var title2 = "Emissão de nota fiscal <br> pode ser simples.";
 var text1 = "A Digital Sistema simplifica: cadastro de fornecedores, relatórios de estoque,controle de compras, gestão de depósitos e mais! Otimize seu negócio conosco. Estamosaqui para tornar a gestão do seu negócio mais eficiente e descomplicada.";
 var text2 = "Com a Digital Sistema, sua agilidade para emitir os três principais tipos de notasfiscais (NFe, NFSe e NFCe) é incomparável. Elimine a necessidade de inserir dados repetidamente, deixe que os impostos sejam calculados automaticamente e desfrute demuito mais praticidade! Descubra como podemos otimizar o seu processo. Clique agorano botão abaixo para saber mais.";
 
