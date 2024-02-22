a66343975bb6e1df8786e6d597b255388f17b59b
Antônio Henrique Pacheco Costa <102102220+henriquef9@users.noreply.github.com>
2024-02-22 20:05:09 -0300

    op

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 
 window.addEventListener('scroll', function() {
   var navbar = document.querySelector('.nav-desktop');
+  
   if (window.pageYOffset > 45) {
       navbar.classList.add('sticky');
   } else {
@@ -10,14 +11,11 @@ window.addEventListener('scroll', function() {
 });
 
 
-function servico() {
-  var btns = document.querySelectorAll('.grup')
-}
-
-
 
 function adicionarClasse() {
   var meuElemento = document.getElementById("container-location");
+  var menu = document.getElementById('menu-tablet');
+  var navbar = document.querySelector('.nav-desktop');
 
   if (window.innerWidth < 768) { 
     meuElemento.classList.add("container-fluid");
@@ -26,7 +24,67 @@ function adicionarClasse() {
     meuElemento.classList.add("container");
     meuElemento.classList.remove("container-fluid");
   }
+
+  if(window.innerWidth >= 992){
+    menu.classList.remove('open');
+    navbar.classList.remove('nav2');
+  }
+
 }
 
 window.addEventListener("load", adicionarClasse);
 window.addEventListener("resize", adicionarClasse);
+
+document.addEventListener("DOMContentLoaded", function() {
+
+  var btnMenuDesktop = document.getElementById('btn-menu-tablet');
+  var menuDesktop = document.getElementById('menu-tablet');
+  var navbarDesktop = document.querySelector('.nav-desktop');
+
+  var btnMenuMobile = document.getElementById('btn-menu-mobile');
+  var menuMobile = document.getElementById('menu-mobile');
+  var navbarMobile = document.querySelector('.nav-mobile');
+
+
+  var filhoBtnDesktop = btnMenuDesktop.querySelector('img');
+  var filhoBtnMobile = btnMenuMobile.querySelector('img');
+
+
+
+  function toggleMenu(btnMenu, menu, navbar){
+    console.log('function 1')
+
+    btnMenu.addEventListener('click', function() {
+      menu.classList.toggle('open');
+
+      if(navbar.classList.contains('nav-desktop')){
+        navbar.classList.toggle('nav2');
+      }
+
+    });
+  }
+
+  function verificarEventoFora(btnMenu, filhoBtn, menu){
+    // Fecha o menu se o usuário clicar fora dele
+    document.addEventListener('click', function(event) {
+      console.log('function 2')
+
+      var isClickInsideMenu = menu.contains(event.target);
+      var isClickOnMenuToggle = (event.target === btnMenu);
+      
+      if (!isClickInsideMenu && !isClickOnMenuToggle && !filhoBtn == event.target) {
+        menu.classList.remove('open');
+      }
+    });
+  }
+
+  toggleMenu(btnMenuDesktop, menuDesktop, navbarDesktop);
+
+  verificarEventoFora(btnMenuDesktop, filhoBtnDesktop, menuDesktop);
+
+  toggleMenu(btnMenuMobile, menuMobile, navbarMobile);
+
+  verificarEventoFora(btnMenuMobile, filhoBtnMobile, menuMobile);
+
+
+});
