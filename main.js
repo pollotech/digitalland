4b7c1937b350b5481b17c3b8c76f96a26ef5f4b5
Antônio Henrique Pacheco Costa <102102220+henriquef9@users.noreply.github.com>
2024-02-22 16:21:42 -0300

    opa

--- main.js
+++ main.js
@@ -13,3 +13,20 @@ window.addEventListener('scroll', function() {
 function servico() {
   var btns = document.querySelectorAll('.grup')
 }
+
+
+
+function adicionarClasse() {
+  var meuElemento = document.getElementById("container-location");
+
+  if (window.innerWidth < 768) { 
+    meuElemento.classList.add("container-fluid");
+    meuElemento.classList.remove("container");
+  } else { 
+    meuElemento.classList.add("container");
+    meuElemento.classList.remove("container-fluid");
+  }
+}
+
+window.addEventListener("load", adicionarClasse);
+window.addEventListener("resize", adicionarClasse);
